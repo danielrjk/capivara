12a136ee82a82221453a40fba086e29ab0cae8b8
Daniel <danielrjk2004@gmail.com>
2023-10-22 02:50:00 -0300

    professor

---
+++ backend/routes/professor.js
@@ -0,0 +1,35 @@
+const express = require("express")
+
+const {
+    recebeProfessores,
+    recebeProfessor,
+    criaProfessor,
+    deletaProfessor,
+    editaProfessor,
+} = require("../controllers/professorController")
+
+const router = express.Router()
+
+// GET -> recebe todos professores
+router.get("/", recebeProfessores)
+
+// GET -> recebe um professor por id
+router.get("/:id", recebeProfessor)
+
+// POST -> cria um professor
+router.post("/", criaProfessor)
+
+// DELETE -> deleta um profesor por id
+router.delete("/:id", deletaProfessor)
+
+// PATCH -> edita um professor por id
+router.patch("/:id", editaProfessor)
+
+// POST -> realiza login no sistema
+//* router.post("/login", loginSistema)
+
+// POST -> realiza logout no sistema
+//* router.post("/logout", logoutSistema)
+
+
+module.exports = router
